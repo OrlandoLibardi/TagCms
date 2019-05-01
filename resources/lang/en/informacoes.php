97c3ca68911c37ff7169f86e60e172b8e27afd4e
Orlando Libardi <orlando.libardi@gmail.com>
2019-05-01 11:42:17 -0300

    Novos campos

--- resources/lang/en/informacoes.php
+++ resources/lang/en/informacoes.php
@@ -1,14 +1,17 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| OlCms Tags Lines
+| OlCms Tags Lines 
 |--------------------------------------------------------------------------
 */
-return [    
-    'tag_email' => 'email@email.com.br',
-    'tag_phone' => '11 9541-20109',
-    'tag_facebook_link' => '',
-    'tag_twitter_link' => '',
-    'tag_instagram_link' => '',   
-    'tag_location_link' => '',   
+
+return [
+'tag_email' => 'contato@ctrld.com.br', 
+'tag_phone' => '11 9541-20109', 
+'tag_twitter_link' => 'https://twitter.com/', 
+'tag_instagram_link' => 'http://instagram.com/', 
+'tag_email_two' => 'talentos@ctrld.com.br', 
+'tag_endress' => 'R. dos Democratas, 591 - Vila Monte Alegre<br />São Paulo - SP, 04305-000', 
+'tag_facebook_link' => 'http://facebook.com/', 
+'tag_vimeo_link' => 'https://vimeo.com/', 
 ];
